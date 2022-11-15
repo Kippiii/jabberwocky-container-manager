80490017587ad4d508aa2f1148ce324194831c3b
Kippiii <ianorzel@gmail.com>
2022-11-15 14:44:56 -0500

    Finishing setting up fully-working demo

--- run.py
+++ run.py
@@ -1,27 +1,11 @@
 import logging
-from sys import stdin, stdout
+from sys import stdin, stdout, argv
 from subprocess import Popen
 import time
 
 from src.containers.container_manager_client import ContainerManagerClient
-from src.containers.container_manager_server import ContainerManagerServer
 from src.system.syspath import get_server_addr_file
 from src.cli.cli import JabberwockyCLI
-
-
-class MyInStream:
-    logger: logging.Logger
-
-    def __init__(self, logger: logging.Logger):
-        self.logger = logger
-
-    def read(self, *args, **kwargs):
-        inp = stdin.read(*args, **kwargs)
-        # inp = self.file.read(*args, **kwargs)
-        if len(inp) > 0 and ord(inp) == 4:
-            return ""
-        self.logger.info("Read %d", ord(inp) if len(inp) == 1 else -1)
-        return inp
 
 
 def main():
@@ -32,18 +16,14 @@ def main():
     # Start the server
     if not get_server_addr_file().is_file():
         logger.debug('Starting Server...')
-        Popen('python server.py')
+        Popen('python server.py', shell=True)
         time.sleep(1)
 
 
-    logger.info("Please input commands: ")
     cli = JabberwockyCLI(stdin, stdout)
     cli.container_manager = ContainerManagerClient()
-    while True:
-        stdout.write("> ")
-        stdout.flush()
-        inp = stdin.readline()
-        cli.parse_cmd(inp)
+    inp = " ".join(argv[1:])
+    cli.parse_cmd(inp)
 
     # Halt server
     # TODO
