d4894ca6b6383659a003b0b81d73de07147e53c2
Dylan <Dylanalexandermcdougall@gmail.com>
2023-01-27 16:18:57 -0500

    FIx bad paths/urls

--- download_prerequisites.py
+++ download_prerequisites.py
@@ -2,28 +2,32 @@ import shutil
 from pathlib import Path
 from urllib.request import urlretrieve
 from sys import platform
-from os import listdir, rmdir, remove
+from os import listdir, rmdir, remove, makedirs
 
-CONTRIB_PATH = Path(__file__).parent
+CONTRIB_PATH = Path(__file__).parent / "contrib"
 
-if not (CONTRIB_PATH / "filezilla/").exists():
+if not CONTRIB_PATH.exists():
+    makedirs(CONTRIB_PATH)
+
+if not (CONTRIB_PATH / "filezilla").exists():
     print("Downaloding Filezilla...")
+    makedirs(CONTRIB_PATH / "filezilla")
 
     if platform == "win32":
-        url = "https://dl3.cdn.filezilla-project.org/client/FileZilla_3.63.1_win64.zip?h=ANIvQYbsJsgefHI_ff-mXA&x=1674844216"
+        url = "https://github.com/dmcdo/jabberwocky-contrib/blob/master/win32filezilla.zip?raw=true"
         downlaod_dest = CONTRIB_PATH / "download.zip"
         extract_dest = CONTRIB_PATH / "tmp/"
-        final_dest = CONTRIB_PATH / "filezilla/"
+        final_dest = CONTRIB_PATH / "filezilla"
         urlretrieve(url, downlaod_dest)
         shutil.unpack_archive(downlaod_dest, extract_dest)
         shutil.move(extract_dest / listdir(extract_dest)[0], final_dest)
         rmdir(extract_dest)
         remove(downlaod_dest)
     elif platform == "linux":
-        url = "https://dl3.cdn.filezilla-project.org/client/FileZilla_3.63.1_x86_64-linux-gnu.tar.bz2?h=S3DdlU_RmKhMSSW2MH14bA&x=1674844216"
+        url = "https://github.com/dmcdo/jabberwocky-contrib/blob/master/linuxfilezilla.tar.bz2?raw=true"
         downlaod_dest = CONTRIB_PATH / "download.tar.bz2"
         extract_dest = CONTRIB_PATH / "tmp/"
-        final_dest = CONTRIB_PATH / "filezilla/"
+        final_dest = CONTRIB_PATH / "filezilla"
         urlretrieve(url, downlaod_dest)
         shutil.unpack_archive(downlaod_dest, extract_dest)
         shutil.move(extract_dest / listdir(extract_dest)[0], final_dest)
