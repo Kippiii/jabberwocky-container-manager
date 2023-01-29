ef62f63e8c7e2649ddd5894aa15b11967be89ed6
Dylan <Dylanalexandermcdougall@gmail.com>
2023-01-28 22:17:35 -0500

    Fix arguments

--- src/system/filezilla.py
+++ src/system/filezilla.py
@@ -4,21 +4,23 @@ from subprocess import Popen, run, DETACHED_PROCESS
 from src.system.syspath import get_container_id_rsa
 
 def filezilla(user: str, pswd: str, host: str, port: str):
+    args = f"sftp://{user}:{pswd}@{host}:{port}"
+
     if getattr(sys, 'frozen', False):
         base = Path(sys.executable).parent.parent / "contrib" / "filezilla"
     else:
         base = Path(__file__).parent.parent.parent / "contrib" / "filezilla"
 
     if sys.platform == "win32":
-        Popen([base / "filezilla.exe", sftp], creationflags=DETACHED_PROCESS)
+        Popen([base / "filezilla.exe", args], creationflags=DETACHED_PROCESS)
     elif sys.platform == "linux":
-        Popen([base / "bin" / "filezilla", sftp])
+        Popen([base / "bin" / "filezilla", args])
     elif sys.platform == "darwin":
         Popen([
             Path("/usr/bin/open"),
             base / "FileZilla.app",
             "--args",
-            sftp
+            args
         ])
     else:
         raise RuntimeError(f"Unknown Platform {sys.platform}")
