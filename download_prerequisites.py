c56e0a9b24acf5301cef8983ea57754221aa849d
Dylan <Dylanalexandermcdougall@gmail.com>
2023-01-30 16:41:28 -0500

    Fix spelling mistakes

--- download_prerequisites.py
+++ download_prerequisites.py
@@ -10,35 +10,35 @@ if not CONTRIB_PATH.exists():
     makedirs(CONTRIB_PATH)
 
 if not (CONTRIB_PATH / "filezilla").exists():
-    print("Downaloding Filezilla...")
+    print("Downloading Filezilla...")
 
     if platform == "win32":
         url = "https://github.com/dmcdo/jabberwocky-contrib/blob/master/win32filezilla.zip?raw=true"
-        downlaod_dest = CONTRIB_PATH / "download.zip"
+        download_dest = CONTRIB_PATH / "download.zip"
         extract_dest = CONTRIB_PATH / "tmp/"
         final_dest = CONTRIB_PATH / "filezilla"
-        urlretrieve(url, downlaod_dest)
-        shutil.unpack_archive(downlaod_dest, extract_dest)
+        urlretrieve(url, download_dest)
+        shutil.unpack_archive(download_dest, extract_dest)
         shutil.move(extract_dest / listdir(extract_dest)[0], final_dest)
         rmdir(extract_dest)
-        remove(downlaod_dest)
+        remove(download_dest)
     elif platform == "linux":
         url = "https://github.com/dmcdo/jabberwocky-contrib/blob/master/linuxfilezilla.tar.bz2?raw=true"
-        downlaod_dest = CONTRIB_PATH / "download.tar.bz2"
+        download_dest = CONTRIB_PATH / "download.tar.bz2"
         extract_dest = CONTRIB_PATH / "tmp/"
         final_dest = CONTRIB_PATH / "filezilla"
-        urlretrieve(url, downlaod_dest)
-        shutil.unpack_archive(downlaod_dest, extract_dest)
+        urlretrieve(url, download_dest)
+        shutil.unpack_archive(download_dest, extract_dest)
         shutil.move(extract_dest / listdir(extract_dest)[0], final_dest)
         rmdir(extract_dest)
-        remove(downlaod_dest)
+        remove(download_dest)
     elif platform == "darwin":
         url = "https://github.com/dmcdo/jabberwocky-contrib/blob/master/FileZilla_3.63.1_macosx-x86.app.tar.bz2?raw=true"
-        downlaod_dest = CONTRIB_PATH / "download.tar.bz2"
+        download_dest = CONTRIB_PATH / "download.tar.bz2"
         extract_dest = CONTRIB_PATH / "filezilla"
-        urlretrieve(url, downlaod_dest)
-        shutil.unpack_archive(downlaod_dest, extract_dest)
-        remove(downlaod_dest)
+        urlretrieve(url, download_dest)
+        shutil.unpack_archive(download_dest, extract_dest)
+        remove(download_dest)
 
 else:
     print("Skipping Filezilla.")
