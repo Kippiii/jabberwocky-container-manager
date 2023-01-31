463c5da7e0893eefbc91a335c03d5dbf64408810
Dylan <Dylanalexandermcdougall@gmail.com>
2023-01-31 14:06:41 -0500

    Edit type hint onf SpinningThread init

--- src/system/multithreading.py
+++ src/system/multithreading.py
@@ -18,7 +18,7 @@ class SpinningTask:
     target: Callable[[], None]
     args: Iterable
 
-    def __init__(self, prompt: str, target: Callable[[], None], args: Iterable = (), out_stream: TextIO = sys.stdout):
+    def __init__(self, prompt: str, target: Callable[[], None], args: Iterable = (), out_stream: TextIO = Optional[sys.stdout]):
         self.prompt = prompt
         self.target = target
         self.args = args
