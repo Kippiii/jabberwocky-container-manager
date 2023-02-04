972025f9c0f5ac5e875274019c392c24ca2cb44e
Dylan <Dylanalexandermcdougall@gmail.com>
2023-02-03 19:28:55 -0500

    Make SpinningTask Crtl+C-able

--- src/system/multithreading.py
+++ src/system/multithreading.py
@@ -28,7 +28,7 @@ class SpinningTask:
         """
         Execute the target task.
         """
-        thread = threading.Thread(target=self._task)
+        thread = threading.Thread(target=self._task, args=self.args, daemon=True)
         thread.start()
 
         spinner = ("|", "/", "-", "\\")
