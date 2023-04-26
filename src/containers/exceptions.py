f078912addf10025be6bbf065436a3b97a209bb5
Dylan <Dylanalexandermcdougall@gmail.com>
2023-04-26 13:47:46 -0400

    fix ClientServerSocket

--- src/containers/exceptions.py
+++ src/containers/exceptions.py
@@ -9,6 +9,7 @@ from pexpect import EOF as PexpectEOFException
 from pexpect import TIMEOUT as PexpectTimeoutException
 from pexpect import ExceptionPexpect
 
+from src.system.my_socket import ClientServerSocket
 from src.system.syspath import get_server_log_file
 
 PORT_FAILURE_RE = r"""Could not set up host forwarding rule"""
@@ -113,7 +114,7 @@ class ServerError(RuntimeError):
     :param sock: The socket connection being used
     """
 
-    def __init__(self, sock: "ClientServerSocket"):
+    def __init__(self, sock: ClientServerSocket):
         self.sock = sock
         self._recv()
         self.sock.close()
